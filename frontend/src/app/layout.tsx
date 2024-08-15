eaf2ea1106d70e0b46e2e2db12ce7fca302c33e1
datamonsterr <phamthanhdat17092004@gmail.com>
2024-08-15 22:38:42 +0700

    sign contract in typescript

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,27 +1,83 @@
 "use client";
 
 import "./globals.css";
-import { Navbar } from "../components/custom/navbar";
-import React from "react";
-import AppWalletProvider from "@/components/custom/AppWalletProvider";
+import { cn } from "@/lib/utils";
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuList,
+    navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import React, { useMemo, useState, useEffect } from "react";
+import {
+    ConnectionProvider,
+    WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
 
-require("@solana/wallet-adapter-react-ui/styles.css");
+// Import CSS for WalletMultiButton (prefer `import` over `require`)
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 export default function RootLayout({
     children,
-}: Readonly<{
+}: {
     children: React.ReactNode;
-}>) {
+}) {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+    const network = WalletAdapterNetwork.Devnet;
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+    const wallets = useMemo(
+        () => [
+            // Add your wallet adapters here, e.g.:
+        ],
+        [network]
+    );
 
     return (
         <html lang="en" className="dark">
             <body className="w-screen h-fit">
-                <AppWalletProvider>
-                    <Navbar />
-                    <div id="container" className="w-full h-fit">
-                        {children}
-                    </div>
-                </AppWalletProvider>
+                {
+                    isMounted ?
+                        <ConnectionProvider endpoint={endpoint}>
+                            <WalletProvider wallets={wallets} autoConnect>
+                                <WalletModalProvider>
+                                    <NavigationMenu className="px-10 bg-transparent">
+                                        <NavigationMenuList>
+                                            <div className="w-screen px-20 py-5 flex justify-between">
+                                                <NavigationMenuItem>
+                                                    <a
+                                                        href="/"
+                                                        className={cn(
+                                                            navigationMenuTriggerStyle(),
+                                                            " text-2xl rounded-none py-8 bg-inherit"
+                                                        )}
+                                                    >
+                                                        Home
+                                                    </a>
+                                                </NavigationMenuItem>
+                                                <NavigationMenuItem>
+                                                    <WalletMultiButton />
+                                                </NavigationMenuItem>
+                                            </div>
+                                        </NavigationMenuList>
+                                    </NavigationMenu>
+                                    <div id="container" className="w-full h-fit">
+                                        {children}
+                                    </div>
+                                </WalletModalProvider>
+                            </WalletProvider>
+                        </ConnectionProvider>
+
+                        : null
+                }
             </body>
         </html>
     );
