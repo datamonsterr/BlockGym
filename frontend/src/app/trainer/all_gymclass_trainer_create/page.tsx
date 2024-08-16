3867dc9d842a3111553760c1a26f644e224d86c7
datamonsterr <phamthanhdat17092004@gmail.com>
2024-08-16 12:02:09 +0700

    update

--- frontend/src/app/trainer/all_gymclass_trainer_create/page.tsx
+++ frontend/src/app/trainer/all_gymclass_trainer_create/page.tsx
@@ -2,27 +2,24 @@
 
 import GymClassItem from "@/components/custom/gym-class-item";
 import { GymData } from "@/lib/models";
-import { useEffect, useState } from "react";
-import { PublicKey } from "@solana/web3.js";
+import { useEffect, useMemo, useState } from "react";
 import { useWallet } from "@solana/wallet-adapter-react";
 
 export default function ViewAllGymClass() {
     const { publicKey } = useWallet();
-
-    
     const [gymClasses, setGymClasses] = useState<GymData[]>([]);
-    useEffect(() => {
+    useMemo(() => {
         // Fetch data from an external API
         const fetchData = async () => {
             const res = await fetch(
-                `http://localhost:8000/get-all-gym-classes-data-by-trainer?trainerPubkey=${publicKey?.toString}`
+                `http://localhost:8000/get-all-gym-classes-data-by-trainer?trainerPubkey=${publicKey?.toString()}`
             );
             const json = await res.json();
             setGymClasses(json.data);
         };
 
         fetchData();
-    }, []);
+    }, [publicKey]);
     return (
         <main className="grid grid-flow-row grid-cols-4 gap-10 px-40">
             {gymClasses.map((item, index) => (
